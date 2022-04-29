08dbb2ab81177f84ad85d9f7763255ef1eaddb35
sanketvc <sanketchandanshive1@gmail.com>
2022-04-29 15:23:54 -0400

    I added one file and edited two, I added the MemoBroadcast.java. Can someone who has the art files set the icons of the notification in the MemoBroadcast.java file please and then it should work.

--- android/src/com/ak/spaceshooter/MainMenuActivity.java
+++ android/src/com/ak/spaceshooter/MainMenuActivity.java
@@ -1,5 +1,4 @@
 package com.ak.spaceshooter;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,7 +11,16 @@ import androidx.lifecycle.LiveData;
 import com.ak.spaceshooter.db.Level;
 import com.ak.spaceshooter.db.LevelDatabase;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
 
+import java.util.Calendar;
 import java.util.List;
 
 public class MainMenuActivity extends AppCompatActivity {
@@ -23,6 +31,29 @@ public class MainMenuActivity extends AppCompatActivity {
         final Button playButton = (Button) findViewById(R.id.play);
         playButton.setOnClickListener(this::onClickPlay);
 
+        //NOTIFICATION CODE
+        NotificationChannel();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 15);
+        calendar.set(Calendar.MINUTE, 17);
+        calendar.set(Calendar.SECOND, 00);
+
+        if (Calendar.getInstance().after(calendar)) {
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
+        }
+
+        Intent intent = new Intent(MainMenuActivity.this, MemoBroadcast.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),pendingIntent);
+        }
+        //
+
+
 
     }
     public void onClickPlay(View v){
@@ -31,5 +62,24 @@ public class MainMenuActivity extends AppCompatActivity {
         startActivity(new Intent(this, LevelSelectActivity.class));
 
     }
+
+    //NOTIFICATION CODE
+    private void NotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Space Shooter";
+            String description = "Space Shooter Game";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("Notification", name, importance);
+            channel.setDescription(description);
+
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+
+        }
+    }
+    //
     //sanket was here
 }
